3de9bda8701745a71a646c319ba62707a30344fc
mike0tdv <mihailtodev3@gmail.com>
2024-09-17 20:35:01 +0300

    ij

--- code/main.py
+++ code/main.py
@@ -18,6 +18,23 @@ class Game(pygame.sprite.Sprite):
         self.collision_sprites = pygame.sprite.Group()
         self.setup()
 
+        # gun timer
+        self.can_shoot = True
+        self.shoot_time = 0
+        self.gun_cooldown = 100
+
+    def input(self):
+        if pygame.mouse.get_pressed()[0] and self.can_shoot:
+            Bullet(surf, pos, direction, groups)
+            self.can_shoot = False
+            self.shoot_time = pygame.time.get_ticks()
+
+    def gun_timer(self):
+        if not self.can_shoot:
+            current_time = pygame.time.get_ticks()
+            if current_time - self.shoot_time >= self.gun_cooldown:
+                self.can_shoot = True
+
     def setup(self):
         map = load_pygame(join("data", "maps", "world.tmx"))
         for x,y,image in map.get_layer_by_name("Ground").tiles():
@@ -32,6 +49,7 @@ class Game(pygame.sprite.Sprite):
         for obj in map.get_layer_by_name("Entities"):
              if obj.name == "Player":
                 self.player = Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+                self.gun = Gun(self.player, self.all_sprites)
 
     def run(self):
         while self.running:
@@ -44,12 +62,16 @@ class Game(pygame.sprite.Sprite):
                     self.running = False
 
             # update
+            self.gun_timer()
+            self.input()
+            self.player.move(dt)
             self.all_sprites.update(dt)
 
             # draw
             self.display_surface.fill("black")
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
+            
         pygame.quit()
 
 if __name__ == "__main__":
--- code/player.py
+++ code/player.py
@@ -3,36 +3,40 @@ from settings import *
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, collision_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load(join("images", "player", "down", "0.png")).convert_alpha()
+        self.load_images()
+        self.state, self.frame_index = 'down', 1
+        self.image = pygame.image.load(join('images', 'player', 'down', '0.png')).convert_alpha()
         self.rect = self.image.get_rect(center = pos)
-        self.hitbox_rect = self.rect.inflate(-50, 0)
-
-        # movement
-        self.direction = pygame.math.Vector2()
+        self.hitbox_rect = self.rect.inflate(-60, -90)
+    
+        # movement 
+        self.direction = pygame.Vector2()
         self.speed = 500
         self.collision_sprites = collision_sprites
 
+    def load_images(self):
+        self.frames = {'left': [], 'right': [], 'up': [], 'down': []}
+
+        for state in self.frames.keys():
+            for folder_path, sub_folders, file_names in walk(join('images', 'player', state)):
+                if file_names:
+                    for file_name in sorted(file_names, key= lambda name: int(name.split('.')[0])):
+                        full_path = join(folder_path, file_name)
+                        surf = pygame.image.load(full_path).convert_alpha()
+                        self.frames[state].append(surf)
+
     def input(self):
         keys = pygame.key.get_pressed()
-        self.direction.x = (int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])) * 2
-        self.direction.y = (int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])) * 2
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
         self.direction = self.direction.normalize() if self.direction else self.direction
-        
+
     def move(self, dt):
         self.hitbox_rect.x += self.direction.x * self.speed * dt
-        self.collision("horizontal")
+        self.collision('horizontal')
         self.hitbox_rect.y += self.direction.y * self.speed * dt
-        self.collision("vertical")
+        self.collision('vertical')
         self.rect.center = self.hitbox_rect.center
-
-        # if self.rect.left > WINDOW_WIDTH + 100:
-        #     self.rect.left = -100
-        # if self.rect.right < -100:
-        #     self.rect.right = WINDOW_WIDTH + 100
-        # if self.rect.bottom > WINDOW_HEIGHT + 100:
-        #     self.rect.top = -100
-        # if self.rect.top < -100:
-        #     self.rect.bottom = WINDOW_HEIGHT + 100
 
     def collision(self, direction):
         for sprite in self.collision_sprites:
@@ -44,6 +48,19 @@ class Player(pygame.sprite.Sprite):
                     if self.direction.y > 0: self.hitbox_rect.bottom = sprite.rect.top
                     if self.direction.y < 0: self.hitbox_rect.top = sprite.rect.bottom
 
+    def animate(self, dt):
+        # get state 
+        if self.direction.x != 0:
+            self.state = 'right' if self.direction.x > 0 else 'left'
+        if self.direction.y != 0:
+            self.state = 'down' if self.direction.y > 0 else 'up'
+
+        # animate
+        self.frame_index = self.frame_index + 5 * dt if self.direction else 0
+        self.image = self.frames[self.state][int(self.frame_index) % len(self.frames[self.state])]
+
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
