cb3a1492ee2a7375abb3116dfb5d181ecf1e28f1
mike0tdv <mihailtodev3@gmail.com>
2024-09-20 17:42:58 +0300

    Added score and death screen

--- code/main.py
+++ code/main.py
@@ -12,6 +12,12 @@ class Game(pygame.sprite.Sprite):
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("Vampire survivor")
         self.clock = pygame.time.Clock()
+        self.title_font = pygame.font.Font(join("data", "Font", "Pixeltype.ttf"), 160)
+        self.score_font = pygame.font.Font(join("data", "Font", "Pixeltype.ttf"), 70)
+        self.start_again_font  = pygame.font.Font(join("data", "Font", "Pixeltype.ttf"), 100)
+        self.main_score_font = pygame.font.Font(join("data", "Font", "Oxanium-Bold.ttf"), 85)
+        self.score = 0
+        self.health = 100
 
         # groups
         self.all_sprites = AllSprites()
@@ -36,9 +42,10 @@ class Game(pygame.sprite.Sprite):
         self.impact_sound.set_volume(0.4)
         self.music = pygame.mixer.Sound(join("audio", "music.wav"))
         self.music.set_volume(0.4)
-        self.music.play(-1)
+        # self.music.play(-1)
 
         # setup        
+        self.game_active = False
         self.load_images()
         self.setup()
 
@@ -94,6 +101,7 @@ class Game(pygame.sprite.Sprite):
             for bullet in self.bullet_sprites:
                 collision_sprites = pygame.sprite.spritecollide(bullet, self.enemy_sprites, False, pygame.sprite.collide_mask)
                 if collision_sprites:
+                    self.score += 1
                     self.impact_sound.play()
                     for sprite in collision_sprites:
                         sprite.destroy()
@@ -101,36 +109,87 @@ class Game(pygame.sprite.Sprite):
 
     def player_collision(self):
         if pygame.sprite.spritecollide(self.player, self.enemy_sprites, False, pygame.sprite.collide_mask):
-            self.running = False
+            self.game_active = False
+
+    def end_screen(self):
+        self.all_sprites.empty()
+        self.collision_sprites.empty()
+        self.bullet_sprites.empty()
+        self.enemy_sprites.empty()
+
+        # text
+        self.title_screen = self.title_font.render("Vampire Survivor", True, "White")
+        self.title_screen_rect = self.title_screen.get_rect(center = (640, 100))
+
+        self.show_score = self.score_font.render(f"Score: {self.score}", True, "White")
+        self.show_score_rect = self.show_score.get_rect(center = (640, 180))
+
+        self.play_again = self.start_again_font.render("Press 'space' to play again", True, "White")
+        self.play_again_rect = self.play_again.get_rect(center = (640, 620))
+
+        self.show_player = pygame.image.load(join("images", "enemies", "skeleton", "0.png")).convert_alpha()
+        self.show_player = pygame.transform.scale2x(self.show_player)
+        self.show_player_rect = self.show_player.get_rect(center = (640, 400))
+
+        # display
+        self.display_surface.fill("black")
+        self.display_surface.blit(self.title_screen, self.title_screen_rect)
+        self.display_surface.blit(self.show_score, self.show_score_rect)
+        self.display_surface.blit(self.play_again, self.play_again_rect)
+        self.display_surface.blit(self.show_player, self.show_player_rect)
+
+        pygame.display.update()
+
+    def start_over(self):
+        self.keys = pygame.key.get_pressed()
+        if self.keys[pygame.K_SPACE]:
+            self.score = 0 
+            self.game_active = True
+            self.load_images()
+            self.setup()
+    
+    def show_score_main(self, score):
+        self.display_score = self.main_score_font.render(str(score), True, "white")
+        self.display_score_rect = self.display_score.get_rect(center = (640, 640))
+        pygame.draw.rect(self.display_surface, 'white', self.display_score_rect.inflate(20, 10).move(0, -3), 5, 10)
+        self.display_surface.blit(self.display_score, self.display_score_rect)
 
     def run(self):
         while self.running:
-            # dt
             dt = self.clock.tick() / 1000
-
-            #EVENT LOOP
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                if event.type == self.enemy_event:
-                    Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
-
-            # update
-            self.gun_timer()
-            self.input()
-            self.player.move(dt)
-            self.all_sprites.update(dt)
-            self.bullet_collision()
-            self.player_collision()
-
-            # draw
-            self.display_surface.fill("black")
-            self.all_sprites.draw(self.player.rect.center)
-            pygame.display.update()
         
-        self.display_surface.fill("black")
+            if self.game_active:
+                #EVENT LOOP
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+                    if event.type == self.enemy_event:
+                        Enemy(choice(self.spawn_positions), choice(list(self.enemy_frames.values())), (self.all_sprites, self.enemy_sprites), self.player, self.collision_sprites)
 
+                # update
+                self.gun_timer()
+                self.input()
+                self.player.move(dt)
+                self.all_sprites.update(dt)
+                self.bullet_collision()
+                self.player_collision()
 
+                # draw
+                self.display_surface.fill("black")
+                self.all_sprites.draw(self.player.rect.center)
+                self.show_score_main(self.score)
+                pygame.display.update()
+
+            else:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.running = False
+                
+                self.end_screen()
+                self.start_over()
+                
+
+        pygame.quit()
 if __name__ == "__main__":
     game = Game()
     game.run()
